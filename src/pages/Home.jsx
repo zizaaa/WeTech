d0ee76776e8a2c66cc4ef4d456dd6c80042229e3
ziza <winfourteen@gmail.com>
2023-08-12 20:06:29 +0800

    add scroll animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,17 @@ import { AiOutlineCheckCircle } from 'react-icons/ai'
 import { HiOutlineArrowLeftCircle, HiOutlineArrowRightCircle } from 'react-icons/hi2'
 
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+import Aos from 'aos'
 
 const Home = () => {
     const [currPage, setCurrPage] = useState(0);
+
+    useEffect(()=>{
+        Aos.init()
+    },[])
 
     const testimonials = [
         {
@@ -54,7 +61,7 @@ const Home = () => {
     <section className="">
         {/* hero */}
         <div className='container mx-auto flex items-center w-full h-[80vh] md:px-0 px-10'>
-            <div className="content-container flex-1">
+            <div data-aos="fade-right" className="content-container flex-1">
                 <h1 className="text-5xl font-semibold text-darkerBlue">Crafting Your Vision: WeTech's Static & Dynamic Websites!</h1>
                 <div className='mt-5 mb-10'>
                     <p className='flex items-center mb-3'>
@@ -78,7 +85,7 @@ const Home = () => {
                     Consult NOW!
                 </Link>
             </div>
-            <div className="img-container flex-1 h-full lg:flex items-center  hidden" >
+            <div data-aos="zoom-in" className="img-container flex-1 h-full lg:flex items-center  hidden" >
                 <img src="/hero-img.svg"/>
             </div>
         </div>
@@ -86,14 +93,15 @@ const Home = () => {
         
         {/* projects */}
         <div className='container mx-auto flex flex-col items-center mt-7 md:px-0 px-10'>
-            <div className='text-center flex flex-col items-center justify-center'>
+            <div data-aos="fade-down" className='text-center flex flex-col items-center justify-center'>
                 <p className='bg-[rgba(34,60,151,0.4)] text-darkBlue uppercase font-semibold px-3 py-1 rounded-full mb-3'>Projects</p>
                 <h1 className='text-4xl font-semibold uppercase text-darkerBlue md:w-2/3 w-full'>Journey Unveiled: Our Latest Project Expedition</h1>
             </div>
             {/* projects container */}
             <div className='mt-10 flex flex-col lg:flex-row items-center gap-20 mb-10'>
 
-                <div className='project-1 flex items-center justify-between gap-10 lg:flex-row flex-col-reverse rounded-xl border-b-4 border-white hover:border-orange p-3 relative'>
+                <div data-aos="zoom-in"
+                    className='project-1 flex items-center justify-between gap-10 lg:flex-row flex-col-reverse rounded-xl border-b-4 border-white hover:border-orange p-3 relative'>
                     <Link className='absolute top-0 right-0 text-gray text-xl hover:text-black transition-all ease-in-out duration-300'>
                         <FaExternalLinkAlt/>
                     </Link>
@@ -102,7 +110,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className='project-1 flex items-center justify-between gap-10 lg:flex-row flex-col rounded-xl border-b-4 border-white hover:border-orange p-3 relative'>
+                <div data-aos="zoom-in" className='project-1 flex items-center justify-between gap-10 lg:flex-row flex-col rounded-xl border-b-4 border-white hover:border-orange p-3 relative'>
                     <Link className='absolute top-0 right-0 text-gray text-xl hover:text-black transition-all ease-in-out duration-300'>
                         <FaExternalLinkAlt/>
                     </Link>
@@ -115,11 +123,11 @@ const Home = () => {
 
                {/* project process container */}
                <div className='bg-darkBlue w-full my-10'>
-            <div className='text-center w-full'>
+            <div data-aos="fade-down" className='text-center w-full'>
                 <h1 className='text-4xl font-semibold uppercase text-white pt-10'>Simple Process</h1>
             </div>
             <div className='container mx-auto flex flex-col lg:flex-row items-center gap-10 py-10'>
-                <div className='flex-1 flex flex-col items-center justify-center'>
+                <div data-aos="zoom-in" className='flex-1 flex flex-col items-center justify-center'>
                     <div className='flex items-center justify-center'>
                         <p className='bg-salmon px-4 py-2 rounded-full text-white'>1</p>
                     </div>
@@ -128,7 +136,7 @@ const Home = () => {
                         <p className='text-gray'>Choose whether your website will have fixed content (static) or interactive features (dynamic) based on your business/project goals.</p>
                     </div>
                 </div>
-                <div className='flex-1 flex flex-col items-center justify-center'>
+                <div data-aos="zoom-in" className='flex-1 flex flex-col items-center justify-center'>
                     <div className='flex items-center justify-center'>
                         <p className='bg-cyan px-4 py-2 rounded-full text-white'>2</p>
                     </div>
@@ -137,7 +145,7 @@ const Home = () => {
                         <p className='text-gray'>Contact our team to discuss your website requirements, design preferences, and functionalities. We're here to guide you through the process.</p>
                     </div>
                 </div>
-                <div className='flex-1 flex flex-col items-center justify-center'>
+                <div data-aos="zoom-in" className='flex-1 flex flex-col items-center justify-center'>
                     <div className='flex items-center justify-center'>
                         <p className='bg-violet px-4 py-2 rounded-full text-white'>3</p>
                     </div>
@@ -146,7 +154,7 @@ const Home = () => {
                         <p className='text-gray'>Our experts create your customized website, bringing your vision to life with design, content integration, and necessary features.</p>
                     </div>
                 </div>
-                <div className='flex-1 flex flex-col items-center justify-center'>
+                <div data-aos="zoom-in" className='flex-1 flex flex-col items-center justify-center'>
                     <div className='flex items-center justify-center'>
                         <p className='bg-orange px-4 py-2 rounded-full text-white'>4</p>
                     </div>
@@ -161,12 +169,12 @@ const Home = () => {
         {/* tech stack */}
         <div>
             <div className='container mx-auto md:px-0 px-10'>
-                <div className='text-center flex flex-col items-center justify-center'>
+                <div data-aos="fade-down" className='text-center flex flex-col items-center justify-center'>
                     <p className='bg-[rgba(255,145,77,0.4)] text-orange uppercase font-semibold px-3 py-1 rounded-full mb-3'>Tech Stack</p>
                     <h1 className='text-4xl font-semibold uppercase text-darkerBlue md:w-1/2 w-full'>Digital Mastery: The Heartbeat of Our Tech Infrastructure</h1>
                 </div>
                 <div className='flex items-center flex-col md:flex-row justify-evenly flex-wrap mt-10 gap-10'>
-                    <div className='flex flex-col gap-5 md:w-auto w-full'>
+                    <div data-aos="fade-left" className='flex flex-col gap-5 md:w-auto w-full'>
                         <div className='flex items-center gap-3 text-darkerBlue font-semibold text-xl'>
                             <span>
                                 <AiOutlineCheckCircle/>
@@ -187,7 +195,7 @@ const Home = () => {
                         </div>
                     </div>
 
-                    <div className='flex flex-col gap-5 md:w-auto w-full'>
+                    <div data-aos="zoom-in" className='flex flex-col gap-5 md:w-auto w-full'>
                         <div className='flex items-center gap-3 text-darkerBlue font-semibold text-xl'>
                             <span>
                                 <AiOutlineCheckCircle/>
@@ -208,7 +216,7 @@ const Home = () => {
                         </div>
                     </div>
                     
-                    <div className='flex flex-col gap-5 md:w-auto w-full'>
+                    <div data-aos="fade-right" className='flex flex-col gap-5 md:w-auto w-full'>
                         <div className='flex items-center gap-3 text-darkerBlue font-semibold text-xl'>
                             <span>
                                 <AiOutlineCheckCircle/>
@@ -235,21 +243,21 @@ const Home = () => {
         {/* reviews */}
         <div className='my-20'>
             <div className='container mx-auto md:px-0 px-10'>
-                <div className='text-center flex flex-col items-center justify-center'>
+                <div data-aos="fade-down" className='text-center flex flex-col items-center justify-center'>
                     <p className='bg-[rgba(34,60,151,0.4)] text-darkBlue uppercase font-semibold px-3 py-1 rounded-full mb-3'>Reviews</p>
                     <h1 className='text-4xl font-semibold uppercase text-darkerBlue md:w-1/2 w-full'>Client Raves: Experiencing Excellence Through Their Words</h1>
                 </div>
                 <div className='mt-10'>
                     <div className='bg-darkerBlue sm:py-0 py-5  w-full flex md:flex-row flex-col items-center rounded-lg drop-shadow-lg'>
-                        <div className='flex rounded-lg'>
+                        <div data-aos="zoom-in" className='flex rounded-lg'>
                             <img src={testimonials ? testimonials[currPage].img:''} className='rounded-lg h-[23rem] w-[20rem] drop-shadow-lg'/>
                         </div>
                         <div className='sm:w-2/3 w-full p-5 flex flex-col items-start justify-center'>
-                            <div className='text-orange mb-5'>
+                            <div data-aos="fade-right" className='text-orange mb-5'>
                                 <p className='text-3xl mb-3 font-semibold'>{testimonials ? testimonials[currPage].message:''}</p>
                                 <p className='font-semibold'>{testimonials ? testimonials[currPage].name:''}</p>
                             </div>
-                            <div className='flex items-center gap-5'>
+                            <div data-aos="zoom-in" className='flex items-center gap-5'>
                                 <button onClick={prevReview} className='text-orange text-4xl'>
                                     <HiOutlineArrowLeftCircle/>
                                 </button>
